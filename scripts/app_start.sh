35b0e7e7d79d74413ce6ab560b66bf081afd54c9
Kwan Yupensuk <kwan@mohara.co>
2022-12-07 19:22:17 +0700

    fix deploy

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/server
-# npm start
-# pm2 start npm --name "swaptokenapp" -- start
-# pm2 startup
-# pm2 save
-# pm2 restart all
-# yarn build
-yarn start
+npm start
+pm2 start npm --name "swaptokenapp" -- start
+pm2 startup
+pm2 save
+pm2 restart all
